eb2a1fd8958c766b10890dd4d4480a5e009caf51
Marcos <marcoslsouzadf@gmail.com>
2021-04-20 04:47:46 -0300

    Passagem de parametros pela URL

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -4,14 +4,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/usuario")
 public class IndexController {
 
+	// http://localhost:8080/usuario/?nome=Marcos
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity<String> init() {
-		return new ResponseEntity<>("Olá Usuário REST Spring Boot", HttpStatus.OK);
+	public ResponseEntity<String> init(@RequestParam (value = "nome", defaultValue = "(usuário indefinido)", required = true) String nome) {
+		return new ResponseEntity<>("Olá "+ nome +" REST Spring Boot", HttpStatus.OK);
 	}
 }

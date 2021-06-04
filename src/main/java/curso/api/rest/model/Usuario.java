6a05e3b2645a9af88f1d7a0b761a453fa47e63d1
marcoslsouza <marcoslsouzadf@gmail.com>
2021-06-03 23:57:00 -0300

    Spring Security - Configurando usuario

--- src/main/java/curso/api/rest/model/Usuario.java
+++ src/main/java/curso/api/rest/model/Usuario.java
@@ -1,15 +1,24 @@
 package curso.api.rest.model;
 
-import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
+import javax.persistence.UniqueConstraint;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -19,7 +28,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode
-public class Usuario implements Serializable {
+public class Usuario implements UserDetails {
 	
 	private static final long serialVersionUID = 3710651919511446683L;
 
@@ -40,4 +49,51 @@ public class Usuario implements Serializable {
 	@OneToMany(mappedBy = "usuario", orphanRemoval = true, cascade = CascadeType.ALL)
 	@EqualsAndHashCode.Exclude
 	private List<Telefone> telefones = new ArrayList<Telefone>();
+	
+	// Papeis ou acessos
+	@OneToMany(fetch = FetchType.EAGER) // Sempre que carregar um usuario, também carregar seus acessos.
+	// Nome da tabela que vai ser criada "usuarios_role"
+	@JoinTable(name = "usuarios_role", uniqueConstraints = @UniqueConstraint(
+			columnNames = {"usuario_id", "role_id"}, name = "unique_role_user"),
+			joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id", table = "usuario", unique = false,
+			foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)), 
+			inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id", table = "role", unique = false, 
+			updatable = false, foreignKey = @ForeignKey(name = "role_fk", value = ConstraintMode.CONSTRAINT)))
+	private List<Role> roles;
+
+	// Acessos do usuario
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.roles;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.login;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }

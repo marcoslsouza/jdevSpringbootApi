1d4d023d63456474fa3a3e536ab09cbfa49d8fcd
marcoslsouza <marcoslsouzadf@gmail.com>
2021-06-09 00:27:33 -0300

    Implementacao da classe ImplementacaoUserDetailsService autenticacao de usuario

--- src/main/java/curso/api/rest/repository/UsuarioRepository.java
+++ src/main/java/curso/api/rest/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package curso.api.rest.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,7 @@ import curso.api.rest.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
-
+	
+	@Query("SELECT u FROM Usuario u WHERE u.login = ?1")
+	Usuario findUserByLogin(String login);
 }
